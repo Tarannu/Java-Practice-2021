a81fdb0323964ff3a780793a677a45046d89807b
taranum islam <tarannum.aiub@gmail.com>
2021-03-03 20:10:52 -0500

    task 2 added

--- backup/practice/src/com/company/Main.java
+++ backup/practice/src/com/company/Main.java
@@ -5,37 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int a=10;
-        int b=12;
-        int temp=0;
-        System.out.println("before swapping numbers are "+a+" "+b);
-        //swapping using third variable
-        temp=a;
-        a=b;
-        b=temp;
-        System.out.println("swapped numbers are "+a+" "+b);
-        //swapping using no variable;
-        a=10;
-        b=12;
-        a=a+b;
-        b=a-b;
-        a=a-b;
-        System.out.println("without using swap "+a+" "+b);
-        //scanner output
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter integer number 1");
-        int inp1=sc.nextInt();
-        System.out.println("the inpput byt the user is "+inp1);
-
-        //
-        System.out.println("enter interger number 2 ");
-        int inp2=sc.nextInt();
-        int z=inp1+inp2;
-        int result=z+30;
-        System.out.println("Adding the inputs is "+z+" then adding 30 to it result is "+result);
 
 
+        int[] a=new int[]{1,2,3,4,5};
+        int[] b=new int[]{1,2,3,4,5};
 
+        int[] result=new int[a.length+b.length];
+        for(int i=0;i<a.length;i++){
+            result[2*i]=a[i];
+            result[(2*i)+1]=b[i];
+        }
+        System.out.println(result);
     }
 }
