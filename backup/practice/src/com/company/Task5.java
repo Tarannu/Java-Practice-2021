c5b0aeedfc4211d1b53e57787c965d3f489fe4cc
taranum islam <tarannum.aiub@gmail.com>
2021-03-08 20:53:33 -0500

    task 5 added

---
+++ backup/practice/src/com/company/Task5.java
@@ -0,0 +1,86 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Task5 {
+    public static void main(String[] args){
+        int[] duplicatesArr=new int[]{1,2,5,4,5,5,6,7,8};
+        Arrays.sort(duplicatesArr);
+        int j=0;
+        for(int i=1;i<duplicatesArr.length-1;i++){
+            if(duplicatesArr[j]!=duplicatesArr[i]){
+                j++;
+                duplicatesArr[j]=duplicatesArr[i];
+            }
+        }
+
+        System.out.println("The length of the array after removing duplicates is "+(j+1));
+
+        int[] largestElement=new int[]{1,2,5,8,10,11,6,7};
+        int max=0;
+        int secondMax=0;
+        int min=999;
+        int secondMin=999;
+
+        for(int i=0;i<largestElement.length;i++){
+            if(largestElement[i]>max){
+                max=largestElement[i];
+            }// max=11;
+            if(largestElement[i]<min){
+                min=largestElement[i];
+            }
+
+        }
+        for(int i=0;i<largestElement.length;i++){
+            if(largestElement[i]<max && largestElement[i]>secondMax){
+                secondMax=largestElement[i];
+            }
+            if(largestElement[i]>min && largestElement[i]<secondMin){
+                secondMin=largestElement[i];
+            }
+        }
+        System.out.println("The second max number is "+secondMax);
+        System.out.println("the second min "+secondMin);
+
+        int[][] matric1=new int[][]{{1,2},{2,5}};
+        int[][] matric2=new int[][]{{5,6},{7,8}};
+        int[][] sum= new int[2][2];
+        System.out.println(" matric sum is");
+        for(int i=0;i<matric1.length;i++){
+            for(j=0;j<matric1[0].length;j++){
+                sum[i][j]=matric1[i][j]+matric2[i][j];
+                System.out.println(sum[i][j]+" ");
+            }
+            System.out.println();
+
+        }
+
+        int[] zeroArr=new int[]{1,2,0,5,6,0,7,8,0,10,11};
+
+        j=0;
+        for(int i=0;i<zeroArr.length;i++){
+            if(zeroArr[i]!=0){
+                zeroArr[j++]=zeroArr[i];
+            }
+        }
+        for(int i=j;i<zeroArr.length;i++){
+            zeroArr[i]=0;
+
+        }
+        System.out.println("after moving zeros to end");
+        for(int i=0;i<zeroArr.length;i++) {
+            System.out.println(zeroArr[i]);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
